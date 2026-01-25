c1952b57682c1c1565dc160960f65b26948e1a21
Sebas <sebastian_09_2003.0@hotmail.com>
2026-01-24 21:34:03 -0500

    Create CommandNode.java

---
+++ src/main/java/com/app/soft2projbackend/nodetypes/CommandNode.java
@@ -0,0 +1,25 @@
+package com.app.soft2projbackend.nodetypes;
+
+import com.app.soft2projbackend.ExecutionContext;
+import com.app.soft2projbackend.Nodo;
+import com.app.soft2projbackend.enums.*;
+
+public class CommandNode extends Nodo {
+    private String command;
+    private String outputKey;
+
+    public CommandNode(String name, String command, String outputKey) {
+        super(name, TipoNodo.COMMAND);
+        this.command = command;
+        this.outputKey = outputKey;
+    }
+
+    @Override
+    public void execute(ExecutionContext context) throws Exception {
+        System.out.println("Ejecutando comando: " + command);
+
+        // mock
+        String output = "command output";
+        context.put(outputKey, output);
+    }
+}

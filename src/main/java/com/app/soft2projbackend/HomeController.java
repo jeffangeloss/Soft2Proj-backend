d0a3310cda4d8eecc28a228dc5e7d6adf68e17e8
Sebas <sebastian_09_2003.0@hotmail.com>
2026-01-24 20:48:45 -0500

    Update HomeController.java

--- src/main/java/com/app/soft2projbackend/HomeController.java
+++ src/main/java/com/app/soft2projbackend/HomeController.java
@@ -1,4 +1,10 @@
 package com.app.soft2projbackend;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
 }

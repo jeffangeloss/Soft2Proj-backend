9f65d1582487f0bef57e137c27b10741b185e5ad
Sebas <sebastian_09_2003.0@hotmail.com>
2026-01-24 20:20:24 -0500

    IntelliJ Idea Project Changes

---
+++ src/test/java/com/app/soft2projbackend/Soft2ProjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.soft2projbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soft2ProjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f3c03b762a73bee7a7badf15d664ac49bfe5f12e
Sebas <sebastian_09_2003.0@hotmail.com>
2026-01-28 12:14:24 -0500

    Update WorkflowRepository.java

--- src/main/java/com/app/soft2projbackend/repository/WorkflowRepository.java
+++ src/main/java/com/app/soft2projbackend/repository/WorkflowRepository.java
@@ -1,9 +1,9 @@
 package com.app.soft2projbackend.repository;
 
 import com.app.soft2projbackend.model.Workflow;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkflowRepository extends CrudRepository<Workflow, Long> {
+public interface WorkflowRepository extends JpaRepository<Workflow, Long> {
 }

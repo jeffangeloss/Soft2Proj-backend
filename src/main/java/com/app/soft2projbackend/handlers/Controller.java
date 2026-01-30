2b22757ac95ea3f82cb5733171c21a18f37cf4f0
Sebas <sebastian_09_2003.0@hotmail.com>
2026-01-29 21:14:44 -0500

    Update Controller.java

--- src/main/java/com/app/soft2projbackend/handlers/Controller.java
+++ src/main/java/com/app/soft2projbackend/handlers/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/workflows")
 @CrossOrigin(origins = {
         "http://localhost:3000",
-        "http://192.168.1.20:3000"
+        "http://192.168.5.2:3000"
 })
 public class Controller {
 

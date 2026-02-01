781c47c0080bbefe9c456a343034336b9ded359a
Sebas <sebastian_09_2003.0@hotmail.com>
2026-01-31 20:17:19 -0500

    Nope Types Tag

--- src/main/java/com/app/soft2projbackend/model/Nodo.java
+++ src/main/java/com/app/soft2projbackend/model/Nodo.java
@@ -1,7 +1,21 @@
 package com.app.soft2projbackend.model;
 
+import com.app.soft2projbackend.nodetypes.*;
+import com.fasterxml.jackson.annotation.*;
+
 import java.util.UUID;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = StartNode.class, name = "START"),
+        @JsonSubTypes.Type(value = CommandNode.class, name = "COMMAND"),
+        @JsonSubTypes.Type(value = ConditionalNode.class, name = "CONDITIONAL"),
+        @JsonSubTypes.Type(value = HttpRequestNode.class, name = "HTTP")
+})
 public abstract class Nodo {
     protected String id;
     protected String name;

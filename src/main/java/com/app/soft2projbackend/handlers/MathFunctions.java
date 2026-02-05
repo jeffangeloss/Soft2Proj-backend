ed3136953be02df4b89b040519bd82d3903d0d3c
Sebas <sebastian_09_2003.0@hotmail.com>
2026-02-05 00:21:23 -0500

    Create MathFunctions.java

---
+++ src/main/java/com/app/soft2projbackend/handlers/MathFunctions.java
@@ -0,0 +1,41 @@
+package com.app.soft2projbackend.handlers;
+
+public class MathFunctions {
+    public static int square(int x) {
+        int result = x * x;
+        System.out.println("square result: " + result);
+        return result;
+    }
+
+    public static long factorial(int x) {
+        if (x < 0) {
+            System.out.println("factorial result: 0");
+            return 0;
+        }
+
+        long result = 1;
+        for (int i = 1; i <= x; i++) {
+            result *= i;
+        }
+
+        System.out.println("factorial result: " + result);
+        return result;
+    }
+
+    public static long expo(int x) {
+        if (x < 0) {
+            System.out.println("expo result: 0");
+            return 0;
+        }
+
+        long result = 1L << x;
+        System.out.println("expo result: " + result);
+        return result;
+    }
+
+    public static int add(int num) {
+        int result = num + 10;
+        System.out.println("add result: " + result);
+        return result;
+    }
+}

1e9ee767b309cab6f938289ac8ad7b0ff90f0a97
Sebas <sebastian_09_2003.0@hotmail.com>
2026-02-17 21:32:06 -0500

    Ban debug we

--- src/main/java/com/app/soft2projbackend/handlers/Controller.java
+++ src/main/java/com/app/soft2projbackend/handlers/Controller.java
@@ -36,15 +36,4 @@ public class Controller {
     public String ping() {
         return "pong";
     }
-
-    @PostMapping("/debug")
-    public ExecutionContext debugWorkflow(@RequestBody Flow flow) {
-        System.out.println(flow.getNodes().getFirst().getClass());
-        return engine.run(flow);
-    }
-
-    @GetMapping("/askdebug")
-    public String debugGet() {
-        return "Use POST with JSON body";
-    }
 }

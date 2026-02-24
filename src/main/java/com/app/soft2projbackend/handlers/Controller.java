6724640c8caaa47c437406896e9b68879269b76d
Sebas <sebastian_09_2003.0@hotmail.com>
2026-02-23 19:07:13 -0500

    Update Controller.java

--- src/main/java/com/app/soft2projbackend/handlers/Controller.java
+++ src/main/java/com/app/soft2projbackend/handlers/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = {
         "http://localhost:3000",
         "http://192.168.5.2:3000"
-}) // Escucha solo en http://localhost:8080/api/workflows/run si se va a probar el backend y frontend en una misma pc
+})
 public class Controller {
 
     private final Engine engine;
